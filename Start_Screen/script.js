59b552b6cbf3c1e12efd1d23b12741ca0ad606df
Nicholas <lynnhilife@gmail.com>
2025-05-28 14:56:11 -0400

    Programming Day2
    Co-authored-by: PresleyHe <PresleyHe@users.noreply.github.com>

--- Start_Screen/script.js
+++ Start_Screen/script.js
@@ -7,9 +7,10 @@ const loading = document.querySelector('.Loading')
 // Start Screen Variables
 const title = document.querySelector('.title-screen')
 const startBtn = document.querySelector('.start')
+const startScreen = document.querySelector(".start-screen")
 
-    // Save File Variables
-
+// Save File Variables
+const backBtn =  document.querySelector('.back-to-startBtn')
 const files = document.querySelector('.file-container')
 
 
@@ -19,12 +20,34 @@ updateNum()
 function updateNum() {
     barEl.style.width = idx + "%";
     idx++;
-    if (idx < 101) {
+    if (idx <= 100) {
         setTimeout(updateNum, 20)
+        startScreen.style.display = "none"
+        title.style.display = "none"
+        startBtn.style.display = "none"
     } else {
         loadingContainer.style.display = 'none'
+        startScreen.style.display = "block"
+        title.style.display = "block"
+        title.style.color = "white"
+        startBtn.style.display = "block"
     }
-    return
+
+    // if(idx > 100){
+    //     startScreen.style.display = "block"
+    // }
+    // return
 }
 //------------ LOADING SCREEN END -------------------
 
+// Start Screen
+
+startBtn.addEventListener("click", ()=> {
+         startScreen.style.display = "none"
+        title.style.display = "none"
+        startBtn.style.display = "none"
+        files.style.display = "block"
+})
+
+
+// Save File Screen 
