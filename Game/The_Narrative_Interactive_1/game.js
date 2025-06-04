5eb4a14d5c591bd7c78e67c7b18b062c86408bfc
Nicholas <lynnhilife@gmail.com>
2025-06-04 14:55:28 -0400

    Programming day7
    Co-authored-by: PresleyHe <PresleyHe@users.noreply.github.com>

--- Game/The_Narrative_Interactive_1/game.js
+++ Game/The_Narrative_Interactive_1/game.js
@@ -2,25 +2,50 @@ const textBox = document.querySelector('.text')
 const screenBox = document.querySelector('.screen')
 const inventory = document.querySelector('.inv')
 const inventoryList = document.querySelector('.inv-list')
+const Option1 = document.querySelector("#Option1")
+const Option2 = document.querySelector("#Option2")
 
 // Functions needed: Dialouge, Progression, Autosave, Inventory, Options, Riddle Text box and button, cutscenes(for the endings)
 
 
-//Function to show and hide the first scene 
-function scene1() {
+//Function for the start
+function Start() {
+    //Starting items
+    addKatana()
     //adding div to the screen div
-
+    
     //changing text content of the text div
-
-
+    
+    
     //Potentially add items that could be equipped in 
     //inventory. Could also add a function?
+} 
+Start()
+
+//TEXT 
+function updateText(content){ 
+    textBox.innerHTML = `<p>${content}</p>`;
 }
 
-function inventory() {
-    //Functions to add items after an event in a scene?
-    //maybe and if statement if a scene unfolds?
 
-    //Adding the item to local storage
+
+
+//  Inventory
+const katana = document.getElementById('katana')
+const mask = document.getElementById('mask')
+const helmet = document.getElementById('helmet')
+const crystal = document.getElementById('crystal')
+
+function addKatana() {
+    katana.style.display='flex'
+}
+function addMask() {
+    mask.style.display='flex'
+}
+function addMelmet() {
+    helmet.style.display='flex'
+}
+function addCrystal() {
+    crystal.style.display='flex'
 }
 
