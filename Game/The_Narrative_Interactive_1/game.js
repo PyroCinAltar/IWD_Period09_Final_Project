cf196b3dce8486684524a01eff4b1135fcf041c0
Nicholas <lynnhilife@gmail.com>
2025-06-05 15:45:59 -0400

    Programming Day8
    Co-authored-by: PresleyHe <PresleyHe@users.noreply.github.com>

--- Game/The_Narrative_Interactive_1/game.js
+++ Game/The_Narrative_Interactive_1/game.js
@@ -2,30 +2,9 @@ const textBox = document.querySelector('.text')
 const screenBox = document.querySelector('.screen')
 const inventory = document.querySelector('.inv')
 const inventoryList = document.querySelector('.inv-list')
-const Option1 = document.querySelector("#Option1")
-const Option2 = document.querySelector("#Option2")
+const Option1 = document.querySelector(".option1")
+const Option2 = document.querySelector(".option2")
 
-// Functions needed: Dialouge, Progression, Autosave, Inventory, Options, Riddle Text box and button, cutscenes(for the endings)
-
-
-//Function for the start
-function Start() {
-    //Starting items
-    addKatana()
-    //adding div to the screen div
-    
-    //changing text content of the text div
-    
-    
-    //Potentially add items that could be equipped in 
-    //inventory. Could also add a function?
-} 
-Start()
-
-//TEXT 
-function updateText(content){ 
-    textBox.innerHTML = `<p>${content}</p>`;
-}
 
 
 
@@ -37,15 +16,125 @@ const helmet = document.getElementById('helmet')
 const crystal = document.getElementById('crystal')
 
 function addKatana() {
-    katana.style.display='flex'
+    katana.style.display = 'flex'
 }
 function addMask() {
-    mask.style.display='flex'
+    mask.style.display = 'flex'
 }
 function addMelmet() {
-    helmet.style.display='flex'
+    helmet.style.display = 'flex'
 }
 function addCrystal() {
-    crystal.style.display='flex'
+    crystal.style.display = 'flex'
 }
+
+
+
+
+// Functions needed: Dialouge, Progression, Autosave, Inventory, Options, Riddle Text box and button, cutscenes(for the endings)
+
+let forestText = []
+let shrineText = []
+let entraceText = []
+let caveText = []
+let newText;
+
+//Function for the start
+
+let stage
+
+function Start() {
+    //Starting items
+
+    //adding div to the screen div
+
+    //Starting message
+    updateText(`You awaken in an enchanted forest. Soft light and lavendar mist filters through the sakura blossoms.
+
+Before you stands a small pink kitsune, its eyes watching you carefully, studying your every move.
+
+Kitsune: "Welcome, traveler. You have crossed into another world."
+
+Your gaze falls to the ground, where a gleaming katana lies. You grab it under the sense that you might need it.`)
+    addKatana()
+    
+    
+
+    changeText()
+
+
+    //Potentially add items that could be equipped in 
+    //inventory. Could also add a function?
+}
+Start()
+
+
+if (Start()) {
+    const newText = `“Follow me. There's something important at the shrine.”
+
+(The kitsune gracefully leads you through the forest until you reach an ancient shrine bathed in soft light.)
+
+Kitsune:
+“This place holds many secrets. Tell me, what brings you here?”`
+    changeText(newText)
+}
+//
+
+
+
+
+
+
+
+
+//function for changing the text
+function changeText(newText) {
+    window.addEventListener("keydown", (e) => {
+        if (e.code === 'Space') {
+            updateText(newText)
+        }
+    })
+}
+
+
+//TEXT 
+function updateText(content) {
+    textBox.innerHTML = `<p class="question">${content}</p>`;
+}
+
+function options(question, option1, option2) {
+    let p = document.createElement('p')
+    p.classList.add('question')
+    p.textContent = question
+
+    let button1 = document.createElement('button')
+    button1.classList.add('option')
+    button1.classList.add('option1')
+    button1.classList.add(option1)
+    button1.textContent = option1
+
+    let button2 = document.createElement('button')
+    button2.classList.add('option')
+    button2.classList.add('option2')
+    button2.classList.add(option2)
+    button2.textContent = option2
+
+    textBox.appendChild(p)
+    textBox.appendChild(button1)
+    textBox.appendChild(button2)
+
+}
+
+function optionSubmit() {
+    textBox.innerHTML = ''
+}
+
+function addText(question) {
+    textBox.innerHTML = `<p class="question">${question}</p>
+                    <input type="text" class="input col-sm-3">
+                    <button class="btn">Submit</button> `
+}
+
+
+
 
